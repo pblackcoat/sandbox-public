62ef5a9b7e5c8388d7e64de5255402ba6589d4b7
Travis Young <blackcoat@gmail.com>
2012-05-08 00:09:54 -0700

    Somethign weird happened there...

--- pygletWin1.py
+++ pygletWin1.py
@@ -3,17 +3,18 @@ import pyglet
 window = pyglet.window.Window(resizable=True)
 
 def drawLine(xstart, ystart, xend, yend):
-    
+		pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
+			('v2i', (xstart, ystart, xend, yend))
+			)
 
 @window.event
 def on_draw():
     i = 0
     window.clear()
     while i <= window.width:
-                i = i + 10
-                pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
-                        ('v2i', (i, 0, i, window.height))
-                        )
+       i = i + 10
+       drawLine(i,0,i,window.height)
+    drawLine(0,window.height//2,window.width,window.height//2)
 
 
 		
